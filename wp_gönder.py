44c65c3a76dc0bd1ce22e8105d24ab576c155b46
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2021-07-03 13:22:22 +0300

    Add files via upload

--- wp_gönder.py
+++ wp_gönder.py
@@ -306,9 +306,9 @@ def movingcenter():
     while 1:
         #v = (0.1 + (0.000833 * konum)) #Vmax =0.6
         v = (0.05 + (0.0009167 * konum)) #Vmax=0.6
-        dist=30
+        dist=20
         count=0
-        if konum >= 35:
+        if konum >= 25:
             msg1.velocity.z = 0
             msg1.header.stamp = rospy.get_rostime ()
             msg1.header.frame_id = "local_ned"
@@ -361,7 +361,7 @@ def movingcenter():
                 velocity_pub.publish (msg1)
                 rate.sleep ()
 
-        elif konum < 35:
+        elif konum < 25:
             msg1.velocity.z = 0
             msg1.velocity.y = 0
             msg1.velocity.x = 0
